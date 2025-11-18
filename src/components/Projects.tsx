1e8c8a42f81a6d5f2dbbdc5896b09e720f2da1f5
abdulvahabaa <abdulvahabaa916@gmail.com>
2025-11-18 22:24:30 +0530

    refactor:project seciton

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -19,6 +19,9 @@ import {
   SiCss3,
   SiJavascript,
   SiBootstrap,
+  SiRedis,
+  SiDocker,
+  SiExpress,
 } from 'react-icons/si';
 
 // Define reusable tech stack icons & colors
@@ -41,44 +44,42 @@ const techStacks = {
 // Define projects data
 const projects = [
   {
-    title: 'Aquawatt',
+    title: 'Personal Portfolio',
     description:
-      'A platform for track and monitor the water and electricity in each room or areas. It is a Full-stack and Iot based real time project.',
-    image: '/assets/aquawatt.png',
-    link: 'https://aquawatt-org.web.app/',
-    github: 'https://github.com/dineshit27/aquawatt-org',
+      'My portfolio website, showcasing my professional background and more. Clean, responsive, and shows off my work while keeping things simple and professional.',
+    image: '/assets/img/portfolio.png',
+    link: 'https://abdulvahabaaa.in/',
+    github: 'https://github.com/abdulvahabaa/react-ts-personal-portfolio',
     techStack: [
       { icon: SiHtml5, name: 'HTML', color: '#e34c26' },
       { icon: SiCss3, name: 'CSS', color: '#264de4' },
       techStacks.react,
       techStacks.tailwind,
       techStacks.typescript,
-      techStacks.firebase,
     ],
   },
   {
-    title: 'Brain Tumour Detector',
+    title: 'Node-Mongoose-JWT-APIs-TS',
     description:
-      'My first real freelance project! where I built the website’s front-end. Clean design, responsive UI and user friendly navigation.',
-    image: '/assets/brain.png',
+      'Production-grade REST API built with Node.js, TypeScript, Express, MongoDB, Redis and Docker. Includes Jest, Supertest, test Coverage and Docker Compose.',
+    image: '/assets/img/Backend API.png',
     link: 'https://tumour-spotter-ai.lovable.app',
     github: 'https://github.com/dineshit27/tumour-spotter-ai',
     techStack: [
-      { icon: SiHtml5, name: 'HTML', color: '#e34c26' },
-      { icon: SiCss3, name: 'CSS', color: '#264de4' },
+      techStacks.node,
       techStacks.typescript,
-      techStacks.react,
-      techStacks.tailwind,
-      techStacks.firebase,
+      techStacks.mongodb,
+      { icon: SiExpress, name: 'Express', color: '#000' },
+      { icon: SiCss3, name: 'CSS', color: '#264de4' },
     ],
   },
   {
-    title: 'Portfolio Website',
+    title: 'Modern Portfolio',
     description:
-      'My portfolio website, showcasing my professional background and more. Clean, responsive, and shows off my work while keeping things simple and professional.',
-    image: '/assets/p2.png',
-    link: 'https://m-dinesh-30.web.app/',
-    github: 'https://github.com/dineshit27/portfolio-web-pvt',
+      'This is a modern developer portfolio. Designed with a clean UI/UX, smooth animations, and dynamic content. it is fully responsive and optimized for performance.',
+    image: '/assets/img/modern-portfolio.png',
+    link: 'https://modern-portfolio-eta-eight.vercel.app/',
+    github: 'https://github.com/abdulvahabaa/modern-portfolio',
     techStack: [
       techStacks.next,
       techStacks.react,
@@ -87,48 +88,7 @@ const projects = [
       techStacks.framer,
     ],
   },
-  {
-    title: 'FoodieHub430',
-    description:
-      'A modern restaurant discovery platform that helps food lovers find their perfect dining experience. With an intuitive interface & powerful search capabilities.',
-    image: '/assets/food.png',
-    link: 'https://foodie-hub430.netlify.app/',
-    github: 'https://github.com/dineshit27/FoodieHub430',
-    techStack: [
-      { icon: SiHtml5, name: 'HTML', color: '#e34c26' },
-      { icon: SiCss3, name: 'CSS', color: '#264de4' },
-      { icon: SiJavascript, name: 'JavaScript', color: '#f0db4f' },
-      { icon: SiBootstrap, name: 'Bootstrap', color: '#563d7c' },
-    ],
-  },
-  {
-    title: 'Business Website',
-    description:
-      'A business website showcasing portfolio, services, client testimonials, and contact section with responsive design and modern UI/UX styling.',
-    image: '/assets/jdmweb.png',
-    link: 'https://jdm-studio430.netlify.app/',
-    github: 'https://github.com/dineshit27/jdm-studio430-website',
-    techStack: [
-      { icon: SiHtml5, name: 'HTML', color: '#e34c26' },
-      { icon: SiCss3, name: 'CSS', color: '#264de4' },
-      { icon: SiJavascript, name: 'JavaScript', color: '#f0db4f' },
-      { icon: SiBootstrap, name: 'Bootstrap', color: '#563d7c' },
-    ],
-  },
-  {
-    title: 'Tech-Hub430',
-    description:
-      'It is a E-commerce website which showcase the product price and details, discounts, client testimonials, our services, etc.',
-    image: '/assets/techhub430.png',
-    link: 'https://tech-hub430.netlify.app/',
-    github:
-      'https://github.com/dineshit27/TechnoHacks-Ecommerce-Website-Tech-hub430',
-    techStack: [
-      { icon: SiHtml5, name: 'HTML', color: '#e34c26' },
-      { icon: SiCss3, name: 'CSS', color: '#264de4' },
-      { icon: SiJavascript, name: 'JavaScript', color: '#f0db4f' },
-    ],
-  },
+
 ];
 
 export function Projects() {
